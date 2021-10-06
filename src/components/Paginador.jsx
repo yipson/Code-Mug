6ee7ae7b6f69f3d846905aeeed52719f566b113b
Jorge Luis Olarte Vargas <jolarte8811@gmail.com>
2021-10-06 15:36:48 -0500

    componentes tabla ventas y paguinador

---
+++ src/components/Paginador.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Paginador(){
+return (
+    <div className="center">
+    <div className="pagination">
+      <a href="#">&laquo;</a>
+      <a href="#">1</a>
+      <a href="#" class="active">2</a>
+      <a href="#">3</a>
+      <a href="#">4</a>
+      <a href="#">5</a>
+      <a href="#">6</a>
+      <a href="#">&raquo;</a>
+      </div>
+    </div>
+)
+
+}
+export default Paginador;
