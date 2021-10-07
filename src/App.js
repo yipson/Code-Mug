8b736016c835a31a848614e8dcbc2a65095140a3
Esteban Gomez <unawaretub86@gmail.com>
2021-10-07 08:22:42 -0500

    subiendo act usuario

--- src/App.js
+++ src/App.js
@@ -1,56 +1,9 @@
 import "./App.css";
 import "./normalize.css";
-import "./components/Paginador"
-
-
+import "./layout/Paginador";
 
 function App() {
-  return (
-    <div className="main">
-      <div className="contenedor">
-        <div className="login">
-          {/* <!-- Lado informativo --> */}
-          <div className="izquierda">
-            <h1 className="texto-izquierda">Code Mug</h1>
-            <h2 className="sub-texto-izquierda">
-              "22 Neque porro quisquam est qui dolorem ipsum quia dolor sit
-              amet, consectetur, adipisci velit..."
-            </h2>
-            
-          </div>
-
-          {/* <!-- Lado de Login --> */}
-          <div className="derecha">
-            <div className="derecha-form">
-              <div className="forma">
-                <form className="from">
-                  <input type="email" placeholder={"Email or Username"}></input>
-                  <div className="linea"></div>
-                  {/* <input type="password" placeholder={"Password"}>
-                    {" "}
-                  </input> */}
-                  <div className="linea"></div>
-                </form>
-                <button className="botonLogin">login</button>
-              </div>
-
-              <div className="checkbox2">
-                {/* <input type="checkbox" name={"checkbox2"} id={"checkbox2"} /> */}
-                <label for="checkbox2">Remember Username</label>
-              </div>
-              <div className="enlace">
-                <button className="button">
-                  <a href="hipervinculo.html" className="x enlace">
-                    &lsaquo; Forgot your Password ?
-                  </a>
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <nav>
+        <ul>
+          <a href="#"><button className="button-ventas button-g">Usuarios</button></a>
+          <a href="#"><button className="button-ventas button-g">Vendedores</button></a>
+          <a href="#"><button className="button-ventas button-g">Ventas</button></a>
+          <a href="#"><button className="button_logout button-g">logout</button></a>
+        </ul>
+      </nav>
+            
+        </div>
+    )
+}
+
+export default Header
