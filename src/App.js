e40dcc2a40a4560e18b0e4aa99d367e3462c926c
Esteban Gomez <unawaretub86@gmail.com>
2021-10-07 12:02:07 -0500

    arreglando app

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ListadoVentas from "pages/ListadoVentas";
 import { Login } from "../src/components/Login";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { Layout } from "Layout/Layout";
+import React from "react";
 
 const App = () => {
   return (
