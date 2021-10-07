15b8806f4b7173cf46c7bd76f15bffad41ac707c
Vivian Mejia Sanchez <vivian.mejiasanchez@gmail.com>
2021-10-07 14:14:55 +0200

    ultimos cambios

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
+
 const Header = () => {
     return (
         <div>
             <nav>
         <ul>
-          <a href="#"><button class="button-ventas button-g">Usuarios</button></a>
-          <a href="#"><button class="button-ventas button-g">Vendedores</button></a>
-          <a href="#"><button class="button-ventas button-g">Ventas</button></a>
-          <a href="#"><button class="button_logout button-g">logout</button></a>
+          <a href="#"><button className="button-ventas button-g">Usuarios</button></a>
+          <a href="#"><button className="button-ventas button-g">Vendedores</button></a>
+          <a href="#"><button className="button-ventas button-g">Ventas</button></a>
+          <a href="#"><button className="button_logout button-g">logout</button></a>
         </ul>
       </nav>
             
@@ -16,4 +17,4 @@ const Header = () => {
     )
 }
 
-export default Header
+export default Header;
