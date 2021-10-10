55b5e12d36470b9659975e7428aeff35a12d8efd
Your NameYour <kevin.guevara090@gmail.com>
2021-10-09 19:43:06 -0500

    Modificacion ActualizarVenta

--- src/pages/ActualizarVenta.jsx
+++ src/pages/ActualizarVenta.jsx
@@ -7,61 +7,104 @@ const ActualizarVenta = () => {
   return (
     <div>
       <Header />
-      <div className="info">
-        <div className="titulo-contenedor">
-          <h1>ID Venta</h1>
-          <button id="open" onClick={popup} className="boton-venta button-g x">
-            Actualizar
-          </button>
+      <body className="ventas">
+        <div className="info">
+          <div className="titulo-contenedor">
+            <h1>ID Venta</h1>
+            <button id="open" onClick={popup} className="boton-venta button-g x">
+              Actualizar
+            </button>
 
-          <div id="contenedorpopup" className="contenedor-pop">
-            <div className="popup">
-              <h1>
-                Venta Actualizada Exitosamente{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-checks"
-                  width="44"
-                  height="44"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#2c3e50"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M7 12l5 5l10 -10" />
-                  <path d="M2 12l5 5m5 -5l5 -5" />
-                </svg>
-              </h1>
-              <button className="boton-ver-ventas">Ver Ventas</button>
-              <button className="boton-nueva-venta">Nueva Venta</button>
-              {/* <!-- boton X eliminado --> */}
-              <button id="cerrar" className="cerrar-pop-venta"></button>
+            <div id="contenedorpopup" className="contenedor-pop">
+              <div className="popup">
+                <h1>
+                  Venta Actualizada Exitosamente{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-checks"
+                    width="44"
+                    height="44"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="#2c3e50"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 12l5 5l10 -10" />
+                    <path d="M2 12l5 5m5 -5l5 -5" />
+                  </svg>
+                </h1>
+                <button className="boton-ver-ventas">Ver Ventas</button>
+                <button className="boton-nueva-venta">Nueva Venta</button>
+                {/* <!-- boton X eliminado --> */}
+                <button id="cerrar" className="cerrar-pop-venta"></button>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div>
-          <form cellspacing="6">
-            <div className="controls">
-              <label>
-                <span>Cliente</span>
-                <input className="estilizar" type="text" />
-              </label>
-              <label>
-                <span>Direccion</span>
-                <input className="estilizar" type="text" />
-              </label>
-              <label>
-                <span>Contacto</span>
-                <input className="estilizar" type="text" />
-              </label>
-            </div>
-          </form>
+          <div>
+            <form cellspacing="6">
+              <div className="controls">
+                <label>
+                  <span>Cliente</span>
+                  <input className="estilizar" type="text" />
+                </label>
+                <label>
+                  <span>Direccion</span>
+                  <input className="estilizar" type="text" />
+                </label>
+                <label>
+                  <span>Contacto</span>
+                  <input className="estilizar" type="text" />
+                </label>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
+        <section className="section-ventas">
+                  <table className="ventas">
+                    <thead>
+                      <tr>
+                        <th scope="row">Cantidad</th>
+                        <th>Descripcion</th>
+                        <th>Precio</th>
+                        <th>Total</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                      <td className="separacion">
+                        <input type="Num" placeholder="Escriba el numero"/>
+                      </td>
+                        <td>
+                          <selection>
+                            <select name="Descripcion">
+                              <option>"1"</option>
+
+                              <option>"2"</option>
+
+                              <option>"3"</option>
+                            </select>
+                          </selection>
+                        </td>
+
+                        <td>Price</td>
+                        <td>Total</td>
+                      </tr>
+                    </tbody>
+                    <tfoot className="alinear">
+                      <tr>
+                        <td></td>
+                        <td></td>
+                        <td>Total:</td>
+                        <td>Sub-Total</td>
+                      </tr>
+                    </tfoot>
+                  </table>
+                </section>
+       </body>
 
       <script src="js/popup.js"></script>
       <Paginador />
