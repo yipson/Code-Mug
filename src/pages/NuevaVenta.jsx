b57dd84957137e736ed6f2f8713112a6cd811893
Vivian Mejia Sanchez <vivian.mejiasanchez@gmail.com>
2021-10-19 03:47:42 +0200

    agregar buscador

--- src/pages/NuevaVenta.jsx
+++ src/pages/NuevaVenta.jsx
@@ -4,171 +4,225 @@ import React, { useState, useRef, useEffect } from "react";
 import axios from "axios";
 import { popup } from "../js/popup";
 
-export const NuevaVenta = () => {
-  const [mostrarTabla, setMostrarTabla] = useState(true);
-  const [ventas, setVentas] = useState([]);
+const NuevaVenta = () => {
+  const [ventas, setVentas] = useState("");
+  const [listaVentas, setListaVentas] = useState([]);
+  const [busqueda, setBusqueda] = useState("");
+  const dataForm = useRef("null");
 
-  const url = "http://localhost:3030/productos";
+  const url = "http://localhost:3030/ventas";
 
   useEffect(() => {
     const fetchData = async () => {
       await axios(`${url}`)
         .then((response) => {
           setVentas(response.data);
-          // setListaProductos(response.data);
+          setListaVentas(response.data);
         })
         .catch((error) => console.log(error));
     };
     fetchData();
   }, []);
 
-  useEffect(() => {
-    //obtener lista de productos desde el backend
-    // setProductos(productosBackend);
-  }, []);
-
-  const dataForm = useRef("null");
-
   const enviarFormulario = async (e) => {
     e.preventDefault();
     const datos = new FormData(dataForm.current);
+    console.log(datos);
+  };
 
-    const nuevaVenta = {};
-    datos.forEach((value, key) => {
-      nuevaVenta[key] = value;
+  const buscadorDiv = (e) => {
+    setBusqueda(e.target.value);
+    filtrar(e.target.value);
+    console.log("busqueda: " + e.target.value);
+  };
+
+  const filtrar = (terminoBusqueda) => {
+    let ResultadoBusqueda = listaVentas.filter((elemento) => {
+      if (
+        elemento.nombre
+          .toString()
+          .toLowerCase()
+          .includes(terminoBusqueda.toLowerCase()) ||
+        elemento._id.includes(terminoBusqueda)
+      ) {
+        return elemento;
+      }
     });
-    console.log(nuevaVenta);
-
-    const options = {
-      method: "POST",
-      url: "http://localhost:3030/productos",
-      headers: { "Content-Type": "application/json" },
-      data: { nombre: nuevaVenta.nombre, precio: nuevaVenta.precio },
-    };
-
-    await axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-        //llamar pop-up nuevo producto
-      })
-      .catch(function (error) {
-        console.error(error);
-        //lamar pop-up error nuevo producto
-      });
-
-    setMostrarTabla(true);
+    setVentas(ResultadoBusqueda);
   };
 
   return (
     <div>
       <Header />
       <body className="ventas">
-        <div className="info">
-          <div className="titulo-contenedor">
-            {/* <div className="nueva-venta-div"> */}
-            <h1>NUEVA VENTA</h1>
-            <button onClick={popup} id="open" className="boton-venta button-g">
-              Guardar
-            </button>
-          </div>
-          <div id="contenedorpopup" className="contenedor-pop">
-            <div className="popup">
-              <h1>
-                Venta Agregada{" "}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-checks"
-                  width="44"
-                  height="44"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="#2c3e50"
-                  fill="none"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <path d="M7 12l5 5l10 -10" />
-                  <path d="M2 12l5 5m5 -5l5 -5" />
-                </svg>
-              </h1>
-              <button className="boton-ver-ventas">
-                <a href="/ListadoVentas">Ver Ventas</a>
+        <form ref={dataForm} onSubmit={enviarFormulario}>
+          <div className="info">
+            <div className="titulo-contenedor">
+              {/* <div className="nueva-venta-div"> */}
+              <h1>NUEVA VENTA</h1>
+              <button
+                onClick={popup}
+                id="open"
+                className="boton-venta button-g"
+              >
+                Guardar
               </button>
-              <button className="boton-nueva-venta">Nueva Venta</button>
-              {/* <!-- boton X eliminado --> */}
-              <button id="cerrar" className="cerrar-pop-venta"></button>
             </div>
-            {/* </div> */}
-          </div>
-          <div>
-            {/* Seccion info cliente */}
-            <form cellspacing="6" ref={dataForm} onSubmit={enviarFormulario}>
-              <div className="controls">
-                <label>
-                  <span>Cliente</span>
-                  <input className="estilizar" type="text" />
-                </label>
-                <label>
-                  <span>Direccion</span>
-                  <input className="estilizar" type="text" />
-                </label>
-                <label>
-                  <span>Contacto</span>
-                  <input className="estilizar" type="text" />
-                </label>
+            {/* <div className="contenedor-busqueda">
+            <p className="text-buscar">Buscar:</p> */}
+            {/* <div className="select">
+              <select>
+                <option value="" selected disabled>
+                  Buscar por:
+                </option>
+                <option value="">Id</option>
+                <option value="">Descripcion</option>
+              </select>
+            </div> */}
+            {/* <input type="text" value={busqueda} onChange={buscadorDiv} />
+          </div> */}
+            <div id="contenedorpopup" className="contenedor-pop">
+              <div className="popup">
+                <h1>
+                  Venta Agregada{" "}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="icon icon-tabler icon-tabler-checks"
+                    width="44"
+                    height="44"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="#2c3e50"
+                    fill="none"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 12l5 5l10 -10" />
+                    <path d="M2 12l5 5m5 -5l5 -5" />
+                  </svg>
+                </h1>
+                <button className="boton-ver-ventas">
+                  <a href="/ListadoVentas">Ver Ventas</a>
+                </button>
+                <button className="boton-nueva-venta">Nueva Venta</button>
+                {/* <!-- boton X eliminado --> */}
+                <button id="cerrar" className="cerrar-pop-venta"></button>
               </div>
-            </form>
+              {/* </div> */}
+            </div>
+            <div>
+              {/* Seccion info cliente */}
+              <form cellspacing="6">
+                <div className="controls">
+                  <label>
+                    <span>Cliente</span>
+                    <input
+                      className="estilizar"
+                      type="text"
+                      name="clienteNombre"
+                    />
+                  </label>
+                  <label>
+                    <span>Direccion</span>
+                    <input
+                      className="estilizar"
+                      type="text"
+                      name="clienteDireccion"
+                    />
+                  </label>
+                  <label>
+                    <span>Contacto</span>
+                    <input
+                      className="estilizar"
+                      type="text"
+                      name="clienteContacto"
+                    />
+                  </label>
+                </div>
+              </form>
+            </div>
           </div>
-        </div>
 
-        {/* seccion tabla */}
-        <section className="section-ventas">
-          <table className="ventas">
-            <thead>
-              <tr>
-                <th scope="row">Cantidad</th>
-                <th>Descripcion</th>
-                <th>Precio</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {/* <form cellspacing="6" ref={dataForm} onSubmit={enviarFormulario}> */}
-              <tr>
-                <td className="separacion">
-                  <input type="Num" placeholder="Escriba el numero" />
-                </td>
-                <td>
-                  <input placeholder="Descripcion"></input>
-                </td>
+          {/* seccion tabla */}
+          <section className="section-ventas">
+            <table className="ventas">
+              <thead>
+                <tr>
+                  <th scope="row">Cantidad</th>
+                  <th>Descripcion</th>
+                  <th>Precio</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
 
-                <td></td>
-                <td></td>
-              </tr>
-              {/* seccion total y subtotal */}
-            </tbody>
-            {/* </form> */}
-            <tfoot className="alinear">
-              <tr>
-                <td></td>
-                <td></td>
-                <td>Total:</td>
-              </tr>
-            </tfoot>
-          </table>
-        </section>
+              <tbody>
+                {/* <form cellspacing="6" ref={dataForm} onSubmit={enviarFormulario}> */}
+                {/* <tr>
+                  <td className="separacion">
+                    <input type="num" placeholder="Cantidad" />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      value={busqueda}
+                      onChange={buscadorDiv}
+                      placeholder="Buscar"
+                    />
+                  </td>
+
+                  <td></td>
+                  <td></td>
+                </tr> */}
+                <tr>
+                  <td>
+                    {" "}
+                    <input />
+                  </td>
+                  <td>
+                    <input type="text" />
+                  </td>
+                  <td>
+                    <input type="text" />
+                  </td>
+                  <td>
+                    <input type="text" />
+                  </td>
+                </tr>
+                <tr>
+                  <td>
+                    {" "}
+                    <input />
+                  </td>
+                  <td>
+                    <input type="text" />
+                  </td>
+                  <td>
+                    <input type="text" />
+                  </td>
+                  <td>
+                    <input type="text" />
+                  </td>
+                </tr>
+                {/* seccion total y subtotal */}
+              </tbody>
+
+              <tfoot className="alinear">
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>Total:</td>
+                </tr>
+              </tfoot>
+            </table>
+          </section>
+        </form>
       </body>
       <script src="js/popup.js"></script>
       <Paginador />
     </div>
   );
-};
 
-export default NuevaVenta;
-
-{
   /* <div className="section-ventas nueva-venta-tabla ">
                   <table className="ventas ">
                     <thead>
@@ -208,4 +262,5 @@ export default NuevaVenta;
                     </tfoot>
                   </table>
                 </div> */
-}
+};
+export default NuevaVenta;
