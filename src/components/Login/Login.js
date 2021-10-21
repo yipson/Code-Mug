81c25b29de49e78d042c1d06cf3eb7d4a1585b4e
Your NameYour <kevin.guevara090@gmail.com>
2021-10-21 13:36:37 -0500

    Organizacion (estilos) del login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -36,68 +36,86 @@ function Login() {
 
   return (
     <Container>
-      <div className="izquierda">
-        <h1 className="texto-izquierda">Code Mug</h1>
-        <h2 className="sub-texto-izquierda">
-          "Ofrecemos el mejor servicio, variedad, calidad 
-          y valor en nuestros productos"
-        </h2>
-      </div>
-        <FormGroup>
-          <label>
-            Email:
-          </label>
-          <input
-            className="form-control"
-            name="email"
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Correo electrónico"
-            required
-          />
-        </FormGroup>
-        <FormGroup>
-          <label>
-            Password:
-          </label>
-          <input
-            className="form-control"
-            name="password"
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Password"
-            value={password}
-            required
-          />
-         <ListGroup>
-          <ListGroupItem tag="a" href="/reset">Olvide mi clave</ListGroupItem>
-          <ListGroupItem tag="a" href="/register">Crea tu cuenta</ListGroupItem>
-        </ListGroup>
-        </FormGroup>
-            <div className="botones">
-              <ButtonGroup>
-                <Button
-                  color="primary"
-                  type="submit"
-                  onClick={() => signInEmailAndPassword(email, password)}
-                >
-                  Login
-                </Button>
-                <div className="or"><p>or</p></div>
-                <Button
-                  color="danger"
-                  onClick={signInWithGoogle}
-                >
-                  Login con Google
-                </Button>
-              </ButtonGroup>
+      <div>
+        <div className="main">
+          <div className="contenedor">
+            <div className="login">
+
+              {/* <!-- Lado informativo --> */}
+              <div className="izquierda">
+                <h1 className="texto-izquierda">Code Mug</h1>
+                <h2 className="sub-texto-izquierda">
+                  "Ofrecemos el mejor servicio, variedad, calidad 
+                  y valor en nuestros productos"
+                </h2>
+              </div>
+
+              {/* <!-- Lado de Login --> */}
+              <div className="derecha">
+                <div className="derecha-form">
+                  <div className="forma">
+                      <FormGroup>
+                        <label>
+                          Email:
+                        </label>
+                        <input
+                          className="form-control"
+                          name="email"
+                          type="text"
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          placeholder={"Correo electrónico"}
+                          required
+                        />
+                      </FormGroup>
+
+                      {/* <div className="linea"></div> */}
+
+                      <FormGroup>
+                        <label>
+                          Password:
+                        </label>
+                        <input
+                          className="form-control"
+                          name="password"
+                          type="password"
+                          onChange={(e) => setPassword(e.target.value)}
+                          placeholder={"Password"}
+                          value={password}
+                          required
+                        />
+                      <ListGroup>
+                        <ListGroupItem tag="a" href="/reset">Olvide mi clave</ListGroupItem>
+                        <ListGroupItem tag="a" href="/register">Crea tu cuenta</ListGroupItem>
+                      </ListGroup>
+                      </FormGroup>
+                          <div className="botones">
+                            <ButtonGroup>
+                              <Button
+                                color="primary"
+                                type="submit"
+                                onClick={() => signInEmailAndPassword(email, password)}
+                              >
+                                Login
+                              </Button>
+                              <div className="or"><p>or</p></div>
+                              <Button
+                                color="danger"
+                                onClick={signInWithGoogle}
+                              >
+                                Login con Google
+                              </Button>
+                            </ButtonGroup>
+                          </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
           </div>
+        </div>
+      </div>
 
       <br />
-
-      
-
     </Container>
   );
 }
